58d4d99a1815c9da88458753d1d26b6cc3bf414d
majid_aghamohammd <majid199372@gmail.com>
2022-11-07 23:39:09 +0330

    create basemodels name

--- buno/models.py
+++ buno/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cafe(models.Model):
+    """"""
+    pass
+
+class CafeMenu(models.Model):
+    """"""
+    pass
+
+class Like(models.Model):
+    """"""
+    pass
+
+class Comment(models.Model):
+    """"""
+    pass
+
+class CafeJobs(models.Model):
+    """"""
+    pass
